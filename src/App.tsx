5a49dbd20eb27c3d9724dcb1aec510edea7ed4d9
Shikhar Jaiswal <shikharskj21@gmail.com>
2023-06-07 03:22:05 +0530

    added mui and used mui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import './App.css';
-import { Column } from './components/Column';
-import { SearchBar } from './components/SearchBar';
+import { Box } from "@mui/material";
+import "./App.css";
+import { Column } from "./components/Column";
+import { SearchBar } from "./components/SearchBar";
+import Button from "@mui/material/Button";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <span className="heading">
-            Jirafy Board
-        </span>
-
-        <div>
+      <span className="heading">Jirafy Board</span>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          my: "1rem"
+        }}
+      >
+        <Box sx={{ width: "50%"}}>
           <SearchBar />
-        </div>
-        <div className='container'>
-          <Column tasks={""}/>
-          <Column tasks={""}/>
-          <Column tasks={""}/>
-          <Column tasks={""}/>
-        </div>
+        </Box>
+        <Box sx={{ display: "flex", gap: 2, width: "50%", justifyContent: "end" }}>
+          <Button variant="outlined" color="success">Create Task</Button>
+          <Button variant="outlined" color="error">Deleted Task</Button>
+        </Box>
+      </Box>
+      <div className="container">
+        <Column tasks={""} />
+        <Column tasks={""} />
+        <Column tasks={""} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,7 +1,56 @@
-
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { Avatar } from "@mui/material";
+import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 
 export const Task: React.FC = () => {
-    return <div className="task">
-        Task
-    </div>
-}
+  return (
+    <Card variant="outlined" className="task">
+      <CardContent>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h6" component="div">
+            Title
+          </Typography>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </Box>
+
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions
+        sx={{
+          mt: -2,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          px: 2,
+          pt: 0.25,
+        }}
+      >
+        <Box>SP: 3</Box>
+        <Box sx={{ display: "flex", gap: 1 }}>
+          <IconButton size="small">
+            <BorderColorOutlinedIcon color="primary" />
+          </IconButton>
+          <IconButton size="small">
+            <DeleteOutlinedIcon color="primary" />
+          </IconButton>
+        </Box>
+      </CardActions>
+    </Card>
+  );
+};
